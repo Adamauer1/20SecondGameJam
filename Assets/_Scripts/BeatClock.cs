157202c92123f2a4c72865463abbe81c6238c8e3
Adamauer1 <adamauer1@gmail.com>
2025-11-17 11:12:26 +0100

    quick commit

--- Assets/_Scripts/BeatClock.cs
+++ Assets/_Scripts/BeatClock.cs
@@ -2,28 +2,42 @@ using UnityEngine;
 
 namespace _Scripts
 {
+    [DefaultExecutionOrder(-200)]
     public class BeatClock : MonoBehaviour
     {
         //public static BeatClock Instance { get; private set; }
 
         [SerializeField] private float m_bpm = 120f;
 
+        [SerializeField] private int m_beatsPerMeasure = 4;
+        [SerializeField] private int m_beatUnit = 4;
+        
         private double m_dspStart;
         private float m_secondsPerBeat;
         
-        public float CurrentBeat => (float)((AudioSettings.dspTime - m_dspStart) / m_secondsPerBeat);
+        // public float CurrentBeat => (float)((AudioSettings.dspTime - m_dspStart) / m_secondsPerBeat);
+        public float CurrentBeat { get; private set; }
         
+        public int CurrentMeasure => Mathf.FloorToInt(CurrentBeat / m_beatsPerMeasure);
+        public float BeatInMeasure => CurrentBeat % m_beatsPerMeasure;
         
-        // private void Awake()
-        // {
-        //     Instance = this;
-        // }
+        private void Awake()
+        {
+            // Instance = this;
+            m_secondsPerBeat = 60f / m_bpm;
+        }
 
         private void Start()
         {
             m_secondsPerBeat = 60 / m_bpm;
             m_dspStart = AudioSettings.dspTime;
             
+        }
+
+        private void Update()
+        {
+            double eslapse = AudioSettings.dspTime - m_dspStart;
+            CurrentBeat = (float) (eslapse / m_secondsPerBeat);
         }
     }
 }
